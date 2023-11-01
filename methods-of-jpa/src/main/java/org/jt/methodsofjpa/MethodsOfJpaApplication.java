ede16255ccbe608e5dab6dcc8036a6be65a1f593
Sai <saipranabpatra1234@gmail.com>
2023-11-01 08:16:01 +0530

    23.11.01

--- methods-of-jpa/src/main/java/org/jt/methodsofjpa/MethodsOfJpaApplication.java
+++ methods-of-jpa/src/main/java/org/jt/methodsofjpa/MethodsOfJpaApplication.java
@@ -6,6 +6,9 @@ import java.util.stream.IntStream;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,9 +23,52 @@ public class MethodsOfJpaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var products = getProducts();
+		// var products = getProducts();
 
-		productRepository.saveAll(products);
+		// productRepository.saveAll(products);
+		// System.out.println(productRepository.count());
+		// System.out.println(productRepository.existsById("0f88fd0a-8d6c-424a-9437-bb156d8a5320"));
+		// productRepository.deleteById("0f83fd0a-8d6c-424a-9437-bb156d8a5320");
+		/*
+		 * var result = productRepository.findAll();
+		 * result.forEach(System.out::println);
+		 */
+
+		/*
+		 * var optionalProduct =
+		 * productRepository.findById("68561a44-d748-4aee-821f-1b3e70b9d21c");
+		 * if(optionalProduct.isPresent()) {
+		 * System.out.println(optionalProduct.get());
+		 * } else {
+		 * System.out.println("No data found");
+		 * }
+		 */
+
+		// var optionalProduct = productRepository.findById("68561a44-d748-4aee-821f-1b3e70b9d21c");
+		// if (optionalProduct.isPresent()) {
+		// 	 var product = optionalProduct.get();
+		// 	 product.setProductPrice(product.getProductPrice() + 1000);
+
+		// 	 productRepository.save(product);
+		// } else {
+		// 	System.out.println("No data found");
+		// }
+		
+		// var result = productRepository.findAll(Sort.by(Direction.DESC, "productPrice"));
+		// result.forEach(System.out::println);
+
+		// var optionalProduct = productRepository.findByProductName("Product-91");
+		// if(optionalProduct.isPresent()) {
+		// 	System.out.println(optionalProduct.get());
+		// } else {
+		// 	System.out.println("No data");
+		// }
+
+		// var product =productRepository.findByProductName("Product-91");
+		// System.out.println(product);
+
+		var products = productRepository.findAllByProductPriceBetween(3000, 13400);
+		products.forEach(System.out::println);
 	}
 
 	public List<Product> getProducts() {
@@ -33,7 +79,7 @@ public class MethodsOfJpaApplication implements CommandLineRunner {
 						.productPrice(1050.50 * i)
 						.build())
 				.toList();
-	
+
 	}
 
 }
--- methods-of-jpa/src/main/java/org/jt/methodsofjpa/ProductRepository.java
+++ methods-of-jpa/src/main/java/org/jt/methodsofjpa/ProductRepository.java
@@ -1,7 +1,12 @@
 package org.jt.methodsofjpa;
 
+import java.util.Optional;
+
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, String> {
+    Optional<Product> findByProductName(String productName);
 
+    List<Product> findAllByProductPriceBetween(double startPrice, double endPrice);
 }
