b41d76c5c4c1b83eb62bd92c789eb47ebf92a4b2
Sai <saipranabpatra1234@gmail.com>
2023-11-02 07:53:27 +0530

    23.11.02

--- methods-of-jpa/src/main/java/org/jt/methodsofjpa/MethodsOfJpaApplication.java
+++ methods-of-jpa/src/main/java/org/jt/methodsofjpa/MethodsOfJpaApplication.java
@@ -10,12 +10,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @SpringBootApplication
 @RequiredArgsConstructor
 public class MethodsOfJpaApplication implements CommandLineRunner {
 	private final ProductRepository productRepository;
+	private final ProductHistoryRepository historyRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MethodsOfJpaApplication.class, args);
@@ -44,31 +46,56 @@ public class MethodsOfJpaApplication implements CommandLineRunner {
 		 * }
 		 */
 
-		// var optionalProduct = productRepository.findById("68561a44-d748-4aee-821f-1b3e70b9d21c");
+		// var optionalProduct =
+		// productRepository.findById("68561a44-d748-4aee-821f-1b3e70b9d21c");
 		// if (optionalProduct.isPresent()) {
-		// 	 var product = optionalProduct.get();
-		// 	 product.setProductPrice(product.getProductPrice() + 1000);
+		// var product = optionalProduct.get();
+		// product.setProductPrice(product.getProductPrice() + 1000);
 
-		// 	 productRepository.save(product);
+		// productRepository.save(product);
 		// } else {
-		// 	System.out.println("No data found");
+		// System.out.println("No data found");
 		// }
-		
-		// var result = productRepository.findAll(Sort.by(Direction.DESC, "productPrice"));
+
+		// var result = productRepository.findAll(Sort.by(Direction.DESC,
+		// "productPrice"));
 		// result.forEach(System.out::println);
 
 		// var optionalProduct = productRepository.findByProductName("Product-91");
 		// if(optionalProduct.isPresent()) {
-		// 	System.out.println(optionalProduct.get());
+		// System.out.println(optionalProduct.get());
 		// } else {
-		// 	System.out.println("No data");
+		// System.out.println("No data");
 		// }
 
 		// var product =productRepository.findByProductName("Product-91");
 		// System.out.println(product);
 
-		var products = productRepository.findAllByProductPriceBetween(3000, 13400);
-		products.forEach(System.out::println);
+		// var products = productRepository.findAllByProductPriceBetween(3000, 13400);
+		// products.forEach(System.out::println);
+		// var products = productRepository.getProduct(1050.50, "Brand-1");
+		// System.out.println(products);
+
+		// int affectedRow = productRepository.updatePrice(6000,
+		// "5cecb780-46ad-4c27-bb49-b4d9f34c4935");
+		// System.out.println(affectedRow);
+
+		transactionConcept();
+
+	}
+
+	@Transactional
+	public void transactionConcept() {
+		var productId = "5cecb780-46ad-4c27-bb49-b4d9f34c4935";
+		productRepository.deleteById(productId);
+		boolean b = false;
+		if (b)
+			throw new RuntimeException();
+
+		var history = ProdcutHistory.builder()
+				.productId(productId)
+				.build();
+		historyRepository.save(history);
 	}
 
 	public List<Product> getProducts() {
--- methods-of-jpa/src/main/java/org/jt/methodsofjpa/ProductRepository.java
+++ methods-of-jpa/src/main/java/org/jt/methodsofjpa/ProductRepository.java
@@ -4,9 +4,19 @@ import java.util.Optional;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, String> {
     Optional<Product> findByProductName(String productName);
 
     List<Product> findAllByProductPriceBetween(double startPrice, double endPrice);
+
+    @Query("SELECT p FROM Product p WHERE p.productBrand=:brand AND p.productPrice=:price")
+    List<Product> getProduct(@Param("price") double price, @Param("brand") String brand);
+
+    @Modifying
+    @Query("UPDATE Product p SET p.productPrice=:price WHERE p.productId = :id")
+    int updatePrice(@Param("price") double productPrice, @Param("id") String productId);
 }
