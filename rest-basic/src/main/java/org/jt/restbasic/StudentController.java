85511950f9b12f068b45abc17196770e59ebb560
Sai <saipranabpatra1234@gmail.com>
2023-11-08 07:58:02 +0530

    23.08.11

--- rest-basic/src/main/java/org/jt/restbasic/StudentController.java
+++ rest-basic/src/main/java/org/jt/restbasic/StudentController.java
@@ -1,16 +1,36 @@
 package org.jt.restbasic;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @CrossOrigin
 public class StudentController {
+
     @GetMapping("/student")
-    @ResponseBody
-    public Student getStudent() {
+    public Student getStudent(@RequestHeader String y_name) {
+        System.out.println(y_name);
         return new Student(101, "smruti", 3456);
     }
+
+    @GetMapping("/hello")
+    public Map<String, ?> hello() {
+        return Map.of("name", "smruti", "email", "snayak@g.com", "roll", 101);
+    }
+
+    @GetMapping("/students")
+    public List<Student> getstudents() {
+        var student1 = new Student(101, "smruti", 3456);
+        var student2 = new Student(201, "smruti", 3456);
+        var student3 = new Student(301, "smruti", 3456);
+        var student4 = new Student(401, "smruti", 3456);
+        return List.of(student1, student2, student3, student4);
+    }
+
 }
